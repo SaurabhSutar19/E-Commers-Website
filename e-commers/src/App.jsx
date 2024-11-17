2edeff2a6c01845f16bebe7b330fd04768ad3fc8
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-17 17:41:11 +0530

    remove unwanted files

--- e-commers/src/App.jsx
+++ e-commers/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Products from "./Components/Products";
+import Contact from "./Components/Contact";
+import SingleProduct from "./Components/SingleProduct";
+import Cart from "./Components/Cart";
+import ErrorPage from "./Components/ErrorPage";
 
 function App() {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
