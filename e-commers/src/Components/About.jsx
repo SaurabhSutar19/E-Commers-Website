aa6a1e85babd1528e9f4222ec70066eb76fe5359
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-20 08:59:23 +0530

    add HeroSection Components and create data object and pass name throw the HeroSecton components

--- e-commers/src/Components/About.jsx
+++ e-commers/src/Components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HeroSection from "./HeroSection";
 
 const About = () => {
-  return <div>About</div>;
+  const data = {
+    name: "E-Commerce Shop",
+  };
+  return (
+    <>
+      <HeroSection myData={data} />
+    </>
+  );
 };
 
 export default About;
