4446d4b2492cdaa723a3214e7e89f1e05e7d72e4
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-22 16:01:16 +0530

    Update ProductContext.jsx for create useReducer hook and add theier code

--- e-commers/src/context/ProductContext.jsx
+++ e-commers/src/context/ProductContext.jsx
@@ -1,13 +1,42 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import Products from "../Components/Products";
+import reducer from "../reducer/productReduce";
 
 // Create the Context
 const AppContext = createContext();
+const API = "https://api.pujakaitem.com/api/products";
+
+const initialstate = {
+  isLoading: false,
+  isError: false,
+  Products: [],
+  featureProducts: [],
+};
 
 // Create a Provider component
 const AppProvider = ({ children }) => {
-  const value = "Saurabh"; // Context value
+  const [state, dispatch] = useReducer(reducer, initialstate);
 
-  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+  // Fetch products inside useEffect
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
+  };
+
+  useEffect(() => {
+    getProducts(API); // Fetch products when the component mounts
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 
 // Custom hook for consuming the context
