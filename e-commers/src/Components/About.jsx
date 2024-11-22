a3814628b1fd0bf03d379d90dba56ad677ab6db9
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-22 11:37:59 +0530

    Update About.jsx for how work UseContext Api

--- e-commers/src/Components/About.jsx
+++ e-commers/src/Components/About.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { useAppContext } from "../context/productcontext";
 import HeroSection from "./HeroSection";
 
 const About = () => {
+  const myname = useAppContext();
   const data = {
     name: "E-Commerce Shop",
   };
   return (
     <>
+      {myname}
       <HeroSection myData={data} />
     </>
   );
