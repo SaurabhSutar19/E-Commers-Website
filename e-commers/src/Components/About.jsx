d471f6712110fea9dbb5a331838d77aeadf785ef
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-24 20:09:57 +0530

    remove unwanted code

--- e-commers/src/Components/About.jsx
+++ e-commers/src/Components/About.jsx
@@ -1,14 +1,11 @@
-import { useAppContext } from "../context/productcontext";
 import HeroSection from "./HeroSection";
 
 const About = () => {
-  const myname = useAppContext();
   const data = {
     name: "E-Commerce Shop",
   };
   return (
     <>
-      {myname}
       <HeroSection myData={data} />
     </>
   );
