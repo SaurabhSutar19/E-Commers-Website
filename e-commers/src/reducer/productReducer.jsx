3e5a3e846a6fa43fce090a772f85ccda7ba704bd
Saurabh19 <saurabhsutar0620@gmail.com>
2024-11-24 21:12:47 +0530

    recorrect the ProductReduct spell

---
+++ e-commers/src/reducer/productReducer.jsx
@@ -0,0 +1,55 @@
+import SingleProduct from "../Components/SingleProduct";
+
+const ProductReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case "SET_API_DATA":
+      const featureData = action.payload.filter((curElem) => {
+        return curElem.featured === true;
+      });
+
+      return {
+        ...state,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featureData,
+      };
+
+    case "API_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    case "SET_SINGLEPRODUCT_LOADING":
+      return {
+        ...state,
+        isSingleLoading: true,
+      };
+
+    case "SET_SINGLEPRODUCT_DATA":
+      return {
+        ...state,
+        isSingleLoading: false,
+        SingleProduct: action.payload,
+      };
+
+    case "SET_SINGLEPRODUCT_ERROR":
+      return {
+        ...state,
+        isSingleLoading: false,
+        isError: true,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default ProductReducer;
